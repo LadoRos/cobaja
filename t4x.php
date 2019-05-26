c7db31f9ca853c9bc03bcf767d4a78c46f8c5356
LadoRos <50670525+LadoRos@users.noreply.github.com>
2019-05-26 21:51:39 +0700

    Add files via upload

---
+++ t4x.php
@@ -0,0 +1,95 @@
+
+<?php
+     {
+
+     echo("<.................................>\n");
+     echo("       aku sayang kamu\n");
+     echo("<.................................>\n");
+     echo("/n");
+     }
+  for($a =0;$a<10;$a++)
+
+
+$day = date ("D");
+switch ($day) {
+ case 'Sun' : $hari = "Minggu"; break;
+ case 'Mon' : $hari = "Senin"; break;
+ case 'Tue' : $hari = "Selasa"; break;
+ case 'Wed' : $hari = "Rabu"; break;
+ case 'Thu' : $hari = "Kamis"; break;
+ case 'Fri' : $hari = "Jum'at"; break;
+ case 'Sat' : $hari = "Sabtu"; break;
+ default : $hari = "Kiamat";
+}
+echo("\n");
+echo "Hari ini hari adalah hari <b>$hari</b>\n";
+echo("\n");
+echo("\n");
+
+
+{
+echo("silahkan masukan user anda\n");
+echo"user :";
+$user =trim(fgets(STDIN));
+$execute =($user);
+print($execute1);
+echo("\n");
+echo("hallo $user, selamat datang di MR.T4X tools\n");
+echo("loading.");
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(">");
+echo("\n");
+echo("\n");
+
+}
+
+for($a=0;$a<10;$a++)
+{
+echo("=> hallo : $user => data kamu telah terhapus\n");
+sleep (1);
+flush ();
+
+echo("$a");
+echo("\n");
+echo("\n");
+}
+echo("JANGAN MARAH MIN :V\n");
+{
+echo("hallo ,coba 3+3x3+3 berapa  ");
+echo ("jawabanmu = ");
+$jawabanmu =trim(fgets(STDIN));
+$execute =($jabanmu);
+print($execute);
+echo("\n");
+echo("\n");
+sleep(1);
+
+echo("###");
+sleep(1);
+
+echo("###");
+sleep(1);
+
+echo("###");
+sleep(1);
+
+echo("###\n");
+sleep(1);
+echo("salah goblok,jawabanku 36\n");
+echo("karena 3+3=6 x 3+3=6 = 36\n");
+echo("hahaha");
+}
+ ?> 
