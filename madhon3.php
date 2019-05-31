d22e41452344773098799e37910a3e0b2256b108
LadoRos <50670525+LadoRos@users.noreply.github.com>
2019-05-31 12:41:49 +0700

    Add files via upload

---
+++ madhon3.php
@@ -0,0 +1,115 @@
+<?php
+{
+echo -n ("masukan username dan password \n[+]username :");
+$user =trim(fgets(STDIN));
+$execute =($user);
+print($execute1);
+
+
+
+echo("[+]password :");
+$pw =trim(fgets(STDIN));
+$executr =($pw);
+print($execute2);
+echo"TUNGGU 3 DETIK";
+
+sleep(3);
+echo "\033[32m
+  ================================================================
+  ==                                                            ==
+  ==              __  __ ____        _____ _  _                 ==
+  ==             |  \/  |  _ \      |_   _| || |__  __          ==
+  ==             | |\/| | |_) |       | | | || |\ \/ /          ==
+  ==             | |  | |  _ <   _    | | |__   _>  <           ==
+  ==             |_|  |_|_| \_\ (_)   |_|    |_|/_/\_\          ==
+  ==                                                            ==
+  ==                                                            ==
+  ================================================================\n";
+  
+
+
+  for($a =0;$a<10;$a++)
+
+$day = date ("D");
+switch ($day) {
+ case 'Sun' : $hari = "Minggu"; break;
+ case 'Mon' : $hari = "Senin"; break;
+ case 'Tue' : $hari = "Selasa"; break;
+ case 'Wed' : $hari = "Rabu"; break;
+ case 'Thu' : $hari = "Kamis"; break;
+ case 'Fri' : $hari = "Jum'at"; break;
+ case 'Sat' : $hari = "Sabtu"; break;
+ default : $hari = "Kiamat";
+}
+echo("\n");
+echo "Hari ini hari adalah hari  = $hari ";
+echo date("d-M-Y\n");
+echo("\n");
+
+
+{
+echo("silahkan masukan user anda\n");
+echo"user :";
+$user =trim(fgets(STDIN));
+$execute =($user);
+print($execute1);
+echo("\n");
+echo("hallo $user, selamat datang di MR.T4X tools\n");
+echo("loading.");
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(".");
+sleep(1);
+echo(">");
+echo("\n");
+
+}
+
+for($a=0;$a<10;$a++)
+{
+echo("=> hallo : $user => data kamu telah terhapus\n");
+sleep (1);
+flush ();
+
+echo("$a");
+echo("\n");
+echo("\n");
+}
+echo("JANGAN MARAH MIN :V\n");
+{
+echo("masukan username dan password anda\n[+]username :");
+$user =trim(fgets(STDIN));
+$execute =($user);
+print($execute1);
+
+
+
+
+echo("[+]password :");
+$pass =trim(fgets(STDIN));
+$executr =($pass);
+print($execute2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
